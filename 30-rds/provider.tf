452f8874cd7ce4ba5243dc6d73f3794f6bb1d490
parijathaT <parijatha10440@gmail.com>
2025-05-20 23:08:03 +0530

    main

---
+++ 30-rds/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.95.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "parijatha-tf-remote-state-dev"
+    key    = "expense-dev-rds" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region = "us-east-1"
+    dynamodb_table = "expense-remote-state-lock-dev"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
